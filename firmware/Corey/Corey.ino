13290fd7ee57c9d6a277325af83a9aa92b07d895
Corey McCall <coreysmccall@gmail.com>
2025-08-22 10:30:42 -0700

    placeholders

---
+++ firmware/Corey/Corey.ino
@@ -0,0 +1,23 @@
+/*
+ * Corey.ino
+ * Corey's firmware for wedding bracelet.
+ * 
+ * There are two modes:
+ * - The default mode makes the bracelet twinkle
+ * - The second mode is for “dancing”: Most LEDs brightly flash or twinkle only around the time you are moving. When you are still, only the yellow LEDs are on to indicate you are in this mode.
+ * 
+ * Change mode by double tapping the bracelet where it says “tap tap”
+ *
+ *
+ * Corey McCall
+ */
+
+
+void setup() {
+  ;
+}
+
+void loop() {
+  ;
+
+}
---
+++ firmware/Kelly/Kelly.ino
@@ -0,0 +1,19 @@
+/*
+ * Kelly.ino
+ * Kelly's firmware for wedding bracelet.
+ * 
+ * TBD
+ *
+ *
+ * Kelly Byron
+ */
+
+
+void setup() {
+  ;
+}
+
+void loop() {
+  ;
+
+}
